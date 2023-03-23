cacb8165bce9a05b0b0687c6cca14f40d344c409
Mohsen <mohsenmahoski@gmail.com>
2023-03-23 18:38:07 +0430

    Get post endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import axios from "axios";
+
+export interface IPost {
+    userId: number;
+    id:     number;
+    title:  string;
+    body:   string;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getPost(id: number){
+        const resposne = await axios.get<IPost>(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        return resposne.data;
   }
 }
