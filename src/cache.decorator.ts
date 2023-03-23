36cf2dee8ae04e12fb4e80bc58d4071779aa73a2
Mohsen <mohsenmahoski@gmail.com>
2023-03-24 00:34:11 +0430

    Create cache.decorator.ts

---
+++ src/cache.decorator.ts
@@ -0,0 +1,57 @@
+import * as fs from "fs";
+
+const cacheDir = `${__dirname}/../cache/`;
+
+const findContentInCacheDirectory = (contentId: string) => {
+        const contentPath = `${cacheDir}${contentId}.json`;
+        if(!fs.existsSync(contentPath)){
+            return;
+        }
+        const content = fs.readFileSync(contentPath, {
+            encoding: "utf8"
+        });
+        return content;
+}
+
+const createFileContentCache = (contentId:string, data: string) => {
+   fs.writeFileSync(`${cacheDir}${contentId}.json`, data, "utf8");
+}
+
+const removeFileContentCache = (contentId:string) => {
+    const contentPath = `${cacheDir}${contentId}.json`;
+    if(fs.existsSync(contentPath)){
+        fs.unlinkSync(contentPath);
+    }
+ }
+
+export const checkCacheDirectoryExistence = () => {
+   if(fs.existsSync(cacheDir)){
+      return;
+   }
+   fs.mkdirSync(cacheDir);
+}
+
+export const CreateCacheDecorator = (target, name, descriptor) => {
+    const original = descriptor.value;
+    descriptor.value = async function(...args) {
+      const postId = args[0];
+      const content = findContentInCacheDirectory(postId);
+      if(!content){
+        const result = await original.apply(this, args);
+        createFileContentCache(postId, JSON.stringify(result));
+        return result;
+      } else {
+            return JSON.parse(content);
+      }
+    };
+};
+
+export const RemoveCacheDecorator = (target, name, descriptor) => {
+    const original = descriptor.value;
+    descriptor.value = async function(...args) {
+      const postId = args[0];
+      removeFileContentCache(postId);
+      const result = await original.apply(this, args);
+      return result;
+    };
+};
